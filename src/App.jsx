77892e464a650aefdd21c00e046b40864473b0d1
ritaurbani <ritaurbani@gmail.com>
2024-12-11 18:25:15 +0100

    feat:wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import tasks from '../assets/tasks';
+
+
+// const backlogArray = tasks.filter((curTask) => {
+//   return curTask.state === "in_progress" || curTask.state === "backlog"
+// })
+
+// const completedArray = tasks.filter((curTask) => {
+//   console.log(curTask)
+//   return curTask.state === "completed"
+// })
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const completedTasks = tasks.filter(task => task.state === 'completed');
+  const currentTasks = tasks.filter(task => task.state !== 'completed');
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className='header'>
+        <h2>TASK MANAGER</h2>
+      </header>
+      <h3 className='title'> Current Tasks {currentTasks.length}</h3 >
+      <ul>
+        {
+          currentTasks.map((task, index) => {
+            return (
+              <ul className='list'>
+                <li key={index}>{task.title} 
+                  <span className='tag'>{task.state}</span>
+                </li> 
+                <li key={index}><span>Priority:{task.priority}</span></li>
+                <li key={index}><span>Est.Time:{task.estimatedTime}</span></li>
+              </ul>
+            )
+          })
+        }
+      </ul>
+
+      <hr />
+
+      <h3 className='title'> Completed Tasks {completedTasks.length} </h3>
+      <ul>
+        {
+          completedTasks.map((task, index) => {
+            return (
+              <ul>
+                <li key={index}>{task.title}</li>
+                <li key={index}><span>Priority:{task.priority}</span></li>
+                <li key={index}><span>Est.Time:{task.estimatedTime}</span></li>
+              </ul>
+            )
+          })
+        }
+      </ul>
     </>
   )
 }
 
 export default App
+
